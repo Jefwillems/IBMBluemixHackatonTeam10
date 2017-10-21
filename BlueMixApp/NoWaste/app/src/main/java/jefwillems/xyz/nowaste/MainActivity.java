20add3372e6dfc003e334cba76c441038c5f0cec
PieterJanCl <pieterjan.clijsters@gmail.com>
2017-10-21 04:07:27 +0200

    start visual recognition

--- BlueMixApp/NoWaste/app/src/main/java/jefwillems/xyz/nowaste/MainActivity.java
+++ BlueMixApp/NoWaste/app/src/main/java/jefwillems/xyz/nowaste/MainActivity.java
@@ -7,15 +7,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class MainActivity extends AppCompatActivity {
 
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VisualRecognition service = new VisualRecognition(VisualRecognition.VERSION_DATE_2016_05_20);
+        ClassifyOptions classifyOptions = null;
 
-
+        try {
+            classifyOptions = new ClassifyOptions.Builder()
+                    .imagesFile(new File("../../res/drawable/fruitbowl.jpg"))
+                    .build();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ClassifiedImages result = service.classify(classifyOptions).execute();
+        System.out.print(result);
     }
 
 
@@ -28,4 +43,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ResultActivity.class);
         startActivity(intent);
     }
+
+
 }
