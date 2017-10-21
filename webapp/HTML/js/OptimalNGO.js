2c99b142f9a887d55b990fa6e3214db4bac2129d
Quentinvk <quentin.vankesteren.w4012@student.hogent.be>
2017-10-21 13:00:38 +0200

    Created OptimalNGOChoice

---
+++ webapp/HTML/js/OptimalNGO.js
@@ -0,0 +1,50 @@
+﻿var RedCross = {
+    Name: "Red cross",
+    City:"Brussels",
+    Street: "Rue de Stalle",
+    Number: 96,
+    NeededFood: null
+}
+
+var Oxfam = {
+    Name: "Oxfam",
+    City:"Ghent",
+    Street: "Ververijstraat", 
+    number: 17,
+    NeededFood: "apple"
+}
+
+var Poverello = {
+    Name: "Poverello",
+    City: "Ghent",
+    Street: "Komijnstraat",
+    Number: 5,
+    Street: "",
+    NeededFood : null
+
+}
+
+var BroederlijkDelen= {
+    Name: "Broederlijk Delen",
+    City: "Brussels",
+    Street: "Huidevettersstraat",
+    Number: 165,
+    NeededFood : "apple"
+}
+var NGOs = [RedCross, Oxfam, Poverello, BroederlijkDelen];
+
+
+var optimalChoice = function (expirationDate, maxRange, neededGoods, stock) {
+    
+    if (expirationDate.day > 5) {
+        return NGOs.firstChild;
+    }
+    else if (expirationDate.day > 2) {
+        for (var ngo in NGOs) {
+            if (ngo.NeededFood === stock);
+        }
+    } else {
+        return NGOs.firstChild;
+    }
+    return null;
+}
